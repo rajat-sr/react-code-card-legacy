00746725259ca3a30b7ccf84f7be21b991c275fc
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-07-23 19:19:57 +0530

    feat: Create stripe code-card clone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Prism from 'prismjs';
+import './prism.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const code = `const terminal = StripeTerminal.create({
+  onFetchConnectionToken: server.fetchConnectionToken,
+});
+
+const result = await terminal.discoverReaders();
+const reader = result.discoveredReaders[0];
+await terminal.connectReader(reader);
+
+const item = {
+  description: 'High Growth Handbook',
+  amount: 2000,
+  quantity: 1,
+};
+const cart = {
+  lineItems: [item],
+  currency: 'usd',
+};
+
+terminal.setReaderDisplay({ type: 'cart', cart });`;
+
+class App extends React.Component {
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
+  render() {
+    return (
+      <div className="space">
+        <div className="card">
+          <div class="card-shine" />
+          <div class="code-snippet">
+            <pre>
+              <code className="prism language-javascript">{code}</code>
+            </pre>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
